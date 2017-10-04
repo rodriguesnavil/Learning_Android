302db6b921d4f44a9be3dbd97582eb2a68628315
thompsonnaidu <thompsonnaiducm1343@gmail.com>
2017-10-04 11:33:15 +0530

    Added  2 new task one for Layout and other for layout. Two activity added to perofrm this task. Only Layout.xml  file are written

--- app/src/main/java/com/data/learning/learning/MainActivity.java
+++ app/src/main/java/com/data/learning/learning/MainActivity.java
@@ -1,12 +1,15 @@
 package com.data.learning.learning;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.CursorIndexOutOfBoundsException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.data.learning.learning.activitylifecycle.Activity1;
 import com.data.learning.learning.model.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,48 +19,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHelper databaseHelper=new DatabaseHelper(MainActivity.this);
-        databaseHelper.insertData("frcrce","8149","crce@email.com");
-        databaseHelper.insertData("frcrce","9922025939","crce@email.com");
-        Cursor res=databaseHelper.selectdata();
-        String name="",rollno="",email="",phone="";
-        while(res.moveToNext()){
-            rollno=res.getString(0);
-            name=res.getString(1);
-            phone=res.getString(res.getColumnIndex("phone"));
-            email=res.getString(res.getColumnIndex("email"));
-        }
-        Toast.makeText(this, ""+rollno+" "+name+" "+phone+" "+email, Toast.LENGTH_SHORT).show();
-        Log.d("DATA","before update "+rollno+" "+name+" "+phone+" "+email);
-        databaseHelper.updatedata(rollno,"frcrce","8149","crce@email.com");
-        res=databaseHelper.selectdata(rollno);
-        while(res.moveToNext()){
-            rollno=res.getString(0);
-            name=res.getString(1);
-            phone=res.getString(res.getColumnIndex("phone"));
-            email=res.getString(res.getColumnIndex("email"));
-        }
-        Log.d("DATA","after update "+rollno+" "+name+" "+phone+" "+email);
+//        DatabaseHelper databaseHelper=new DatabaseHelper(MainActivity.this);
+//        databaseHelper.insertData("frcrce","8149","crce@email.com");
+//        databaseHelper.insertData("frcrce","9922025939","crce@email.com");
+//        Cursor res=databaseHelper.selectdata();
+//        String name="",rollno="",email="",phone="";
+//        while(res.moveToNext()){
+//            rollno=res.getString(0);
+//            name=res.getString(1);
+//            phone=res.getString(res.getColumnIndex("phone"));
+//            email=res.getString(res.getColumnIndex("email"));
+//        }
+//        Toast.makeText(this, ""+rollno+" "+name+" "+phone+" "+email, Toast.LENGTH_SHORT).show();
+//        Log.d("DATA","before update "+rollno+" "+name+" "+phone+" "+email);
+//        databaseHelper.updatedata(rollno,"frcrce","8149","crce@email.com");
+//        res=databaseHelper.selectdata(rollno);
+//        while(res.moveToNext()){
+//            rollno=res.getString(0);
+//            name=res.getString(1);
+//            phone=res.getString(res.getColumnIndex("phone"));
+//            email=res.getString(res.getColumnIndex("email"));
+//        }
+//        Log.d("DATA","after update "+rollno+" "+name+" "+phone+" "+email);
+//
+//        databaseHelper.delete(rollno);
+//        databaseHelper.insertData("frcrce","8149","crce@email.com");
+//        databaseHelper.insertData("frcrce","9922025939","crce@email.com");
+//        res=databaseHelper.selectdata();
+//        while(res!=null && res.moveToNext()){
+//            rollno=res.getString(0);
+//            name=res.getString(1);
+//            phone=res.getString(res.getColumnIndex("phone"));
+//            email=res.getString(res.getColumnIndex("email"));
+//            Log.d("DATABASE","before delete "+rollno+" "+name+" "+phone+" "+email);
+//        }
+//        res=databaseHelper.login("1 or '1'='1'");
+//        if(res!=null){
+//            Toast.makeText(this, "res", Toast.LENGTH_SHORT).show();
+//            while(res.moveToNext()){
+//                rollno=res.getString(res.getColumnIndex("id"));
+//                name=res.getString(res.getColumnIndex("name"));
+//                Toast.makeText(this,""+rollno+" "+name,Toast.LENGTH_LONG).show();
+//            }
+//        }
+    }
 
-        databaseHelper.delete(rollno);
-        databaseHelper.insertData("frcrce","8149","crce@email.com");
-        databaseHelper.insertData("frcrce","9922025939","crce@email.com");
-        res=databaseHelper.selectdata();
-        while(res!=null && res.moveToNext()){
-            rollno=res.getString(0);
-            name=res.getString(1);
-            phone=res.getString(res.getColumnIndex("phone"));
-            email=res.getString(res.getColumnIndex("email"));
-            Log.d("DATABASE","before delete "+rollno+" "+name+" "+phone+" "+email);
-        }
-        res=databaseHelper.login("1 or '1'='1'");
-        if(res!=null){
-            Toast.makeText(this, "res", Toast.LENGTH_SHORT).show();
-            while(res.moveToNext()){
-                rollno=res.getString(res.getColumnIndex("id"));
-                name=res.getString(res.getColumnIndex("name"));
-                Toast.makeText(this,""+rollno+" "+name,Toast.LENGTH_LONG).show();
-            }
+    public void goToActivity(View view){
+        Intent intent;
+        switch (view.getId()){
+
+            case R.id.actlifecycle: intent=new Intent(getBaseContext(),Activity1.class);
+                                    startActivity(intent);
+                                    break;
         }
     }
 }
---
+++ app/src/main/java/com/data/learning/learning/layout/LayoutActivity.java
@@ -0,0 +1,17 @@
+package com.data.learning.learning.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.data.learning.learning.R;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity);
+    }
+
+
+}
